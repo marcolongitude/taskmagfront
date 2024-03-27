a725e2914ffbee007492a972d8c8840160e73f3b
Marco Aurélio Guimarães <marcocpdti@gmail.com>
2024-03-27 16:15:17 -0300

    add dark theme material ui

--- src/main.tsx
+++ src/main.tsx
@@ -6,27 +6,38 @@ import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./router";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import "./index.css";
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            refetchOnWindowFocus: false,
-            staleTime: 1000 * 60 * 30, //30 segundos
-        },
-    },
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: 1000 * 60 * 30, //30 segundos
+		},
+	},
+});
+
+const darkTheme = createTheme({
+	palette: {
+		mode: "dark",
+	},
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <Provider store={store}>
-                    <PersistGate loading={null} persistor={persistor}>
-                        <AppRoutes />
-                    </PersistGate>
-                </Provider>
-            </BrowserRouter>
-        </QueryClientProvider>
-    </React.StrictMode>
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Provider store={store}>
+					<PersistGate loading={null} persistor={persistor}>
+						<ThemeProvider theme={darkTheme}>
+							<CssBaseline />
+							<AppRoutes />
+						</ThemeProvider>
+					</PersistGate>
+				</Provider>
+			</BrowserRouter>
+		</QueryClientProvider>
+	</React.StrictMode>
 );
