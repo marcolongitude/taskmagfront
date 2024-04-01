87de0fd1df04141c5edec66634a11494202f177a
MarcoAurelio <marcocpdti@gmail.com>
2024-04-01 19:53:10 -0300

    :styles correções gerais lista tarefas (#12)

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,46 +1,47 @@
-import { Box, Breadcrumbs } from "@mui/material";
+import { Box, Breadcrumbs, Typography } from "@mui/material";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import { TypographyST } from "@solterra/components";
 import { useLocation } from "react-router-dom";
 import { ContainerST, LinkST } from "./styled";
 import React from "react";
 
 interface Props {
-    children: React.ReactNode;
-    title: string;
+	children: React.ReactNode;
+	title: string;
 }
 
 export const Container = ({ title, children }: Props) => {
-    const location = useLocation();
-    const pathnames = location.pathname.split("/").filter((x) => x);
+	const location = useLocation();
+	const pathnames = location.pathname.split("/").filter((x) => x);
 
-    return (
-        <ContainerST>
-            <Breadcrumbs
-                separator={<NavigateNextIcon fontSize="small" />}
-                aria-label="breadcrumb"
-            >
-                {pathnames.map((name, index) => {
-                    const routeTo = `/${pathnames
-                        .slice(0, index + 1)
-                        .join("/")}`;
-                    const isLast = index === pathnames.length - 1;
+	return (
+		<ContainerST>
+			<Breadcrumbs
+				separator={<NavigateNextIcon fontSize="small" />}
+				aria-label="breadcrumb"
+			>
+				{pathnames.map((name, index) => {
+					const routeTo = `/${pathnames
+						.slice(0, index + 1)
+						.join("/")}`;
+					const isLast = index === pathnames.length - 1;
 
-                    return isLast ? (
-                        <TypographyST key={routeTo}>{name}</TypographyST>
-                    ) : (
-                        <LinkST to={routeTo} key={routeTo}>
-                            {name}
-                        </LinkST>
-                    );
-                })}
-            </Breadcrumbs>
-            <Box marginTop={2} marginBottom={5}>
-                <TypographyST variant="h4" noWrap color="primary">
-                    {title}
-                </TypographyST>
-            </Box>
-            {children}
-        </ContainerST>
-    );
+					return isLast ? (
+						<Typography color="secondary" key={routeTo}>
+							{name}
+						</Typography>
+					) : (
+						<LinkST to={routeTo} key={routeTo}>
+							{name}
+						</LinkST>
+					);
+				})}
+			</Breadcrumbs>
+			<Box marginTop={2} marginBottom={5}>
+				<Typography variant="h4" noWrap color="secondary">
+					{title}
+				</Typography>
+			</Box>
+			{children}
+		</ContainerST>
+	);
 };
