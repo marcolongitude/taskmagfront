6abe446622ef7d680b14a1cd0a7f59c492205585
Marco Aurélio Guimarães <marcocpdti@gmail.com>
2024-04-02 16:22:23 -0300

    correções types

--- src/types/tasks/types.tasks.ts
+++ src/types/tasks/types.tasks.ts
@@ -1,50 +1,29 @@
+export interface ITarefa {
+	idtasks: string;
+	title: string;
+	description: string;
+	time: number;
+	users_id_users: string;
+	date: string;
+	status: "concluido" | "pendente";
+}
+
 export type IResponseGetTarefasByUser = {
-	data: {
-		idtasks: string;
-		title: string;
-		description: string;
-		time: number;
-		users_id_users: string;
-		date: string;
-		status: "concluido" | "pendente";
-	}[];
+	data: ITarefa[];
 	statusCode: number;
 };
 
 export type IResponseDeleteTarefasByUser = {
-	data: {
-		idtasks: string;
-		title: string;
-		description: string;
-		time: number;
-		users_id_users: string;
-		date: string;
-		status: "concluido" | "pendente";
-	};
+	data: ITarefa[];
 	statusCode: number;
 };
 
 export type IResponseEditTarefasByUser = {
-	data: {
-		idtasks: string;
-		title: string;
-		description: string;
-		time: number;
-		users_id_users: string;
-		date: string;
-		status: "concluido" | "pendente";
-	};
+	data: ITarefa[];
 	statusCode: number;
 };
 
 export type IResponsePostTarefasByUser = {
-	data: {
-		title: string;
-		description: string;
-		time: number;
-		userId: string;
-		date: string;
-		status: "concluido" | "pendente";
-	};
+	data: ITarefa[];
 	statusCode: number;
 };
